50760d4131ba3ae76c21682eac9e599eb8b91772
ymgit19 <ymgithub19@gmail.com>
2025-11-01 12:58:45 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'https://foodapp-server-6kzh.onrender.com';
+  const url = 'https://foodapp-server-2g8f.onrender.com';
   const [token, setToken] = useState('');
   const [food_list, setFoodList] = useState([]);
 
